7c72c8039bc43e757c5114d08ac6d99671b3171c
Parikshit <parik.sonwane06@gmail.com>
2025-08-10 02:13:08 +0530

    setup

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="gitbot",  # must be unique on PyPI
-    version="0.1.0",
+    name="gitbotx",  # must be unique on PyPI
+    version="0.1.1",
     author="Parikshit Sonwane",
     author_email="parik.sonwane06@gmail.com",
     description="A smart CLI tool to automate git workflows and suggest commands",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/parikshit-06/gitbot",
+    url="https://github.com/parikshit-06/gitbotx",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3"
@@ -19,7 +19,7 @@ setup(
     python_requires='>=3.8',
     entry_points={
         'console_scripts': [
-            'gitbot=gitbot.cli:main',
+            'gitbotx=gitbotx.cli:main',
         ],
     },
 )
